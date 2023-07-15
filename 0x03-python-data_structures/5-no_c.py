24516c48af6c48434ffc97ba5d6dcb303074c496
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-15 16:28:42 -0700

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
     new_characters = []
     for i in my_string:
-        if i.lower() not in {'c', 'C'}:#used to filter c/C out
+        if i.lower() not in {'c', 'C'}:
             new_characters.append(i)
     new_string = ''.join(new_characters)
     return (new_string)
