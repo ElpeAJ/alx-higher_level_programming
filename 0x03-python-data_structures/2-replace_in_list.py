c89cd18fa2c8980c1e32802c62114161ce3ccfbd
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-15 12:43:53 -0700

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,5 @@ def replace_in_list(my_list, idx, element):
     if idx < 0:
         return (my_list)
     if (idx > len(my_list) - 1):
-    mylist[idx] = element
+    my_list[idx] = element
     return (my_list)
