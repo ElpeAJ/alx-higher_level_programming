afdf24a9a141dfd426fd1b1397c64ff876238c9c
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-15 12:03:41 -0700

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    count = 0
-    for i in my_list:
-        count += 1
-    for i in range(count):
-        if idx < 0:
-            return (None)
-        if idx >= range(count):
-            return (None)
+count = 0
+for i in my_list:
+    count += 1
+for i in range(count):
+    if idx < 0:
+        return (None)
+    if idx >= range(count):
+        return (None)
     return (my_list[idx])
