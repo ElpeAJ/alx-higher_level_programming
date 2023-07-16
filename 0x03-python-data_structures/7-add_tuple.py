576af6f8bb867c18dcfa5726e14f7ea794e90070
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-16 11:42:12 -0700

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if i in tuple_a:
+    if (len(tuple_a) < 2):
+        tuple_a.append(0)
+    if (len(tuple_b) < 2):
+        tuple_b.append(0)
+    return (tuple_a[i] + tuple_b[i])
