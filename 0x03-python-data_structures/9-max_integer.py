f517798d01680b3c3ae6c8adf5f3a328320d2c60
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-16 11:04:27 -0700

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max_no = 0
+    max_no = -1
 
     if (my_list):
         for i in my_list:
-            if (i >= max_no):
+            if (i > max_no):
                 max_no = i
         return (max_no)
     return (None)
