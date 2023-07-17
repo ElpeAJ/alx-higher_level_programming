959392b21974b44a40d2e2a32edfaa52051cea30
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-17 15:29:43 -0700

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,9 @@
-
+#!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    for i in my_list:
+        if i % 2 == 0:
+            return (True)
+        else:
+            return (False)
+    return (my_list)
