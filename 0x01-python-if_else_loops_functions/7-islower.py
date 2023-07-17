964f3001349ce9dd3d2f1578156fa257df6e0034
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-17 13:20:41 -0700

    Task 7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+i = 0
+def islower(c):
+    if i in range(97, 122, 1):
+        chr(i)
+        return (True)
+    elif i in range(65, 91, 1):
+        chr(i)
+        return (False)
+    else:
+        return (False)
