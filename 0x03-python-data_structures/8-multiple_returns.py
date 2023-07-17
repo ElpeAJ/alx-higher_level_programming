8af477c3ce754bd259d80db7fcd0ba7a8791133a
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-17 14:12:42 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     sentence_tuple = ()
     length_of_string = len(sentence)
     if length_of_string == 0:
-        sentence_tuple = (0, None)
+        sentence_tuple = (length_of_string, None)
     else:
         sentence_tuple = (length_of_string, sentence[0])
     return sentence_tuple
