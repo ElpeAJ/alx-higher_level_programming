7d14d9c1965bd2a4f53a561f50db53dbbb13f7b3
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-19 15:17:32 -0700

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    matrix_square = [list(map(lambda x: x ** 2, per_row)) for per_row in matrix]
+    matrix_square = list(list(map(lambda x: x ** 2, per_row)) for per_row in matrix)
     return matrix_square
 
