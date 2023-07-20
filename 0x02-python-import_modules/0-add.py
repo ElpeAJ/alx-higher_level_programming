871f0345f05f2cbec7b2b82574ecd1f2d7f6a7f0
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-20 08:15:05 -0700

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-
 from add_0 import add
 if (__name__ == "__main__"):
     a = 1
     b = 2
     sum_num = add(a, b)
-    print("{} + {} = {}". format(a, b, sum_num))
+    print("{:d} + {:d} = {:d}". format(a, b, sum_num))
