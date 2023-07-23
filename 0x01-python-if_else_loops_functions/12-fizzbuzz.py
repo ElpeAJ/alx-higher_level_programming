f910ea3fe40e4757748c96960a58240eb54b2425
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-23 14:40:46 -0700

    Fizz Buzz FizzBuzz update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif number % 3 == 0:
             print("Fizz ", end="")
         else:
-            print("{} ".format(number), end ="")
+            print("{} ".format(number), end="")
