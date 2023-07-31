fd108b45a67866450d6eb998216892ffa7dc416d
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-31 15:01:45 -0700

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    quotient = 0
     try:
         quotient = a / b
     except ZeroDivisionError:
@@ -8,3 +9,4 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: None")
         print("{}".format(quotient))
+    return (quotient)
