63b7277a68a859e11d8b3b71f29b944d8ddb473b
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-31 15:40:12 -0700

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,12 +2,13 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     quotient_list = []
+    quotient = 0
     for i in range(list_length):
         quotient = 0
         try:
             quotient = my_list_1[i] / my_list_2[i]
         except TypeError:
-            print("Wrong type")
+            print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
         except IndexError:
