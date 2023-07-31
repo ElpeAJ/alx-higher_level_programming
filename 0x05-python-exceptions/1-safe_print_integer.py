4f3d39bc09244c5088a02275d81e6f902e15e41b
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-31 12:03:36 -0700

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,6 @@ def safe_print_integer(value):
     while value:
         try:
             print("{:d}".format(value))
-            break
             return True
         except ValueError:
             return False
