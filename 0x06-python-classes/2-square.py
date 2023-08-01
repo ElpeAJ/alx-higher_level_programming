71875df6a6e8db4c0f572ab58583b073dd42da69
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-01 11:43:49 -0700

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 
+"""Define a class named Square"""
+
+class Square:
+    """Define a class named Square"""
+    def __init__(self, size=0):
+        if(type(size) is int):
+            if (size < 0):
+                raise ValueError("size must be >= 0")
+            self.__size = size
+            else:
+                raise TypeError("size must be an integer")
