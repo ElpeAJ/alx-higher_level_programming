ec1ac016130a4139fdc3570f38a40c8fb1fd1603
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-01 12:11:04 -0700

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,6 +11,7 @@ class Square:
     """
     def __init__(self, size=0):
         """initialize square"""
+
         if(type(size) is int):
             if (size < 0):
                 raise ValueError("size must be >= 0")
