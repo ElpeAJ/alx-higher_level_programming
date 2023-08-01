ddbb4017a4d2ed83a81e2706e466dda01a5f4ea9
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-01 11:20:14 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-
+"""Defines a class named Square"""
 class Square:
-
     def __init__(self, size):
-
-        self.__size = size
+        """Initialize an instance of a square with size givien"""
+    self.__size = size
