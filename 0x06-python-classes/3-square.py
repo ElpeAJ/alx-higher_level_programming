f163275e2926d68f2b7846d75fab487389f6b77b
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-01 12:06:40 -0700

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3i
+
+"""
+Define a class named Square
+"""
+
+
+class Square:
+    """
+    Define a class named Square
+    """
+    def __init__(self, size=0):
+        """initialize square"""
+        if(type(size) is int):
+            if (size < 0):
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Returns the area for a square"""
+
+        return (self.__size ** 2)
