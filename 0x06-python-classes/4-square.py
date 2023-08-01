1581b8c2b84e0d39d6d227ec2e999bdd5bac40c4
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-01 12:33:32 -0700

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
 
 """
-Define a class named Square
+Define a class named Square that defines a square
 """
 
 
 class Square:
     """
-    Define a class named Square
+    Define a class named Square that defines a square
     """
 
     def __init__(self, size=0):
         """
         Initialize square
+        """
+        self.size = size
+
+    def area(self):
+        """
+        Returns the area for a square
+        """
+
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """
+        return size for the square
+        """
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        Set size of the square
         """
 
         if(type(size) is int):
@@ -21,9 +43,3 @@ class Square:
             self.__size = size
         else:
             raise TypeError("size must be an integer")
-
-    def area(self):
-        """
-        Returns the area for a square
-        """
-        return (self.__size ** 2)
