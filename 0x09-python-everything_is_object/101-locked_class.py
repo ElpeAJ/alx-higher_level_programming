f8602c0fa02fe26c62e67a66df5d978845a163e1
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-09 01:28:22 -0700

    Task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define class: LockedClass"""
+
+
+class LockedClass:
+    """prevents the user from dynamically creating
+    new instance attributes, except if the new instance
+    attribute is called first_name"""
+    __slots__ = ["first_name"]
