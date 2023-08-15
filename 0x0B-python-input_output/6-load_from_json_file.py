407f1ff279a6962144eba0ae7ac9237e30dece08
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-15 13:25:19 -0700

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that creates an Object from a “JSON file”"""
+import json
+
+
+def load_from_json_file(filename):
+    """Argument: filename
+    returns: decoded JSON object that was loaded"""
+    with open(filename, "r") as f:
+        return json.load(f)
