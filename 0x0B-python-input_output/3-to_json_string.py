d1310ab70f0543a28cd12492180504e630cb05c1
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-15 13:01:55 -0700

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a function that returns the JSON representation of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """returns JSON reprensation of string"""
+
+    return (json.dumps(my_obj))
