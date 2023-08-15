2960ca5b38e90b3d30ce66e0de1101682069634b
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-15 13:39:14 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a function that reads a text file (UTF8) and prints it to stdout"""
+"""a function that reads a text file (UTF8)
+and prints it to stdout"""
 
 
 def read_file(filename=""):
