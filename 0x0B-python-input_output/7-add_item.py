e70128dcc43161c8e8e5880fb3bcf669d08fa0fe
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-08-15 14:04:13 -0700

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a script that adds all arguments to a
+Python list, and then save them to a file"""
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+
+def add_items(arg):
+    try:
+        py_list = load_from_json_file("add_item.json")
+    except:
+        py_list = []
+    py_list += arg
+    save_to_json_file(py_list, "add_item.json")
+
+    if __name__ == "__main__":
+        add_item(sys.argv[1:])
