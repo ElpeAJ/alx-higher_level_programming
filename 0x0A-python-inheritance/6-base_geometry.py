f0c8f5550b5cb4704c56a06a369d4e6b77a03378
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-11-07 08:18:20 -0800

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    class defined
+    """
+    def area(self):
+        """
+        Returns area
+        """
+        raise Exception("area() is not implemented")
