45abca6073c218232ded72c2f813fa4dc30ed98e
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-11-07 06:56:46 -0800

    Task 4 update comments

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -17,7 +17,7 @@ def inherits_from(obj, a_class):
     a_class : type to be compared against
 
     Return : True if the object is an instance
-    of a class that inherited from the specified class 
+    of a class that inherited from the specified class
     otherwise False
     """
     return (issubclass(type(obj), a_class) and type(obj) is not a_class)
