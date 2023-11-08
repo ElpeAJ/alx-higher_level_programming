e6657a6692444b25fa7aa72db23ca1b7632539db
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-11-07 21:16:41 -0800

    Made changes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""A script that adds all arguments to a
+"""A function that adds all arguments to a
 Python list, and then save them to a file"""
-import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from sys import argv
+from os.path import isfile
 
+if (__name__ == "__main__"):
+    argc = 0
+    save_json = __import__("5-save_to_json_file").save_to_json_file
+    load_json = __import__("6-load_from_json_file").load_from_json_file
+    my_list = []
+    filename = "add_item.json"
 
-def add_items(args):
-    try:
-        py_list = load_from_json_file("add_item.json")
-    except:
-        py_list = []
-    py_list += args
-    save_to_json_file(py_list, "add_item.json")
+    argc = len(argv)
+    if (isfile(filename)):
+        my_list = load_json(filename)
 
-    if __name__ == "__main__":
-        add_item(sys.argv[1:])
+    if (argc > 1):
+        for index in range(1, argc, 1):
+            my_list.append(argv[index])
+
+    save_json(my_list, filename)
