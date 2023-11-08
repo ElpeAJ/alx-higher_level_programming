e4cb0a344175fdc1f4b9e4398b6a40640aba9220
Elpedia Junior Arthur <129404506+ElpeAJ@users.noreply.github.com>
2023-11-07 20:31:20 -0800

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""a function that returns the JSON representation of an object (string)"""
-import json
-
-
-def to_json_string(my_obj):
-    """returns JSON reprensation of string"""
-
-    return (json.dumps(my_obj))
