d1e6bca4469cf2a9d76eab53d9bf4c186403eff2
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-11-07 21:55:49 -0800

    Made Changes

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,6 +7,18 @@ def pascal_triangle(n):
     n : integer
     """
     myList = []
+    left = 1
+    right = 1
 
     if n <= 0:
         return myList
+    for i in range(n):
+        myList.append([1] * (i + 1))
+
+    for row in range(2, n):
+        for col in range(1, row):
+            left = myList[row - 1][col - 1]
+            right = myList[row - 1][col]
+            myList[row][col] = left + right
+
+    return (myList)
