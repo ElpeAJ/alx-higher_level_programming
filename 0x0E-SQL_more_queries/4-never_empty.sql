b9e82bf8eba82b4bb6547d8855670f6b0f752542
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-12-14 02:22:16 -0800

    Task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
---Write a script that creates the table
---id_not_null on your MySQL server.
+-- Write a script that creates the table
+-- id_not_null on your MySQL server.
 CREATE TABLE IF NOT EXISTS id_not_null
 (
 	id INT DEFAULT 1,
