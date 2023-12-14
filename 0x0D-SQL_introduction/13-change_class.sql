3a6a721af5c12ef35e21b42e6d8c56683725f9ae
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-12-14 10:09:23 -0800

    Task 13

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
--- Write a script that removes all records with a
+-- Write a script that removes all records with 
 -- score <= 5 in the table second_table
 DELETE * FROM second_table
 WHERE score <= 5;
