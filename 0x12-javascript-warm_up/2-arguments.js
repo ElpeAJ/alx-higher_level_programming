48cac8bc497b1953600735bfb08009629bcad3de
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-01-08 05:08:33 -0800

    Put process.argv.length into a variable

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+const argc = process.argv.length;
 
-if (process.argv.length === 2) {
+if (argc === 2) {
 	console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (argc === 3) {
 	console.log('Arguments found');
 } else {
 	console.log('Arguments found');
