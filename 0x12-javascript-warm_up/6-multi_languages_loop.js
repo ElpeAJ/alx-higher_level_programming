0532ae28abc2d806f209ab1c43c2af05374b5d7f
ElpeAJ <elpediaarthurjunior@gmail.com>
2024-01-08 15:56:29 -0800

    A program that loops through an array and print all the elements

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const sentences = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let sentence = 0; sentence <= (sentences.length - 1); sentence++) {
+  console.log(sentences[sentence]);
+}
