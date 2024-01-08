7e5dae700f5b5f0ea3bb1ce95a323c33ec83cf5e
ElpeAJ <elpediaarthurjunior@gmail.com>
2024-01-08 15:25:48 -0800

    Changed  to '' in the concatenating string

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const firstArgc = process.argv[2];
 const secondArgc = process.argv[3];
-console.log(firstArgc + " is " + secondArgc);
+console.log(firstArgc + ' is ' + secondArgc);
