4b1fc89fa00ef4aaf3a412d7c886451ddbe23a3d
ElpeAJ <elpediaarthurjunior@gmail.com>
2024-01-08 17:32:58 -0800

    A program that print a square with tbhe string 'X'  when a number is passed as an argument

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const numArgc = parseInt(process.argv[2]);
+if (isNaN(numArgc)) {
+  console.log('Missing size');
+} else {
+  for (let rargc = 0; rargc < numArgc; rargc++) {
+    for (let argc = 0; argc < numArgc; argc++) {
+      console.log('X');
+  }
+  console.log('');
+  }
+}
